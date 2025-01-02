f0e5485ed95c26989f2f847d8b2fd95df68fba5a
mo-sike <74986573+mo-sike@users.noreply.github.com>
2025-01-03 02:35:31 +0530

    updated automation v2

--- ms_rewards_automation.py
+++ ms_rewards_automation.py
@@ -1,13 +1,32 @@
+"""This Module perform search operation in default browser"""
+
+__version__ = 2.0
+
 import time
-import subprocess
 import webbrowser
+import pyautogui
+
 
 def perform_search():
-    x = 0
-    for x in range(35):
-        url = f"https://www.bing.com/search?q={x}"
-        webbrowser.open_new_tab(url)
-        time.sleep(2)
+    """
+    Automates opening a browser tab and performing the search.
+    """
+    for x in range(1, 35):
+        # Open a new browser tab
+        webbrowser.open_new_tab("https://www.bing.com")
+        time.sleep(3)
+
+        pyautogui.hotkey('alt', 'd')
+        time.sleep(1)
+
+        search_query = f"test_search_{x}"
+        pyautogui.typewrite(search_query)
+        time.sleep(1)
+
+        pyautogui.press('enter')
+        print(f"Performed search: {search_query}")
+        time.sleep(5)
+
 
 if __name__ == "__main__":
     perform_search()
